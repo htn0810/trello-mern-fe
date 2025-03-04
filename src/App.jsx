e06f6b090eccd0b0ef55d6612f64dc9c584f3b5f
htn0810 <htn08102001@gmail.com>
2025-03-04 23:12:33 +0700

    react hook form & login, register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import NotFound from "@/pages/404/NotFound";
+import Auth from "@/pages/Auth/Auth";
 import Board from "@/pages/Boards/_id";
 import { Route, Routes, Navigate } from "react-router-dom";
 
@@ -11,6 +12,8 @@ function App() {
         element={<Navigate to="/boards/67bde4fb91ce8893ccc23f55" replace />}
       />
       <Route path="/boards/:boardId" element={<Board />} />
+      <Route path="/login" element={<Auth />} />
+      <Route path="/register" element={<Auth />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
